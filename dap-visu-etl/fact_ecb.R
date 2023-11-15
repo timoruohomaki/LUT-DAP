72c83487ac3e8c6b02d36d32eb4f6966a7fb635a
Timo Ruohomaki <timo@ruohomaki.fi>
2023-11-15 17:53:56 +0200

    updated ecb country dim

--- dap-visu-etl/fact_ecb.R
+++ dap-visu-etl/fact_ecb.R
@@ -1,9 +1,18 @@
 install.packages("ecb")
+
 library(ecb)
 library(dplyr)
 library(lubridate)
 library(RPostgreSQL)
 library(odbc)
+
+apause <- function(x)
+{
+        p1 <- proc.time()
+        Sys.sleep(x)
+        proc.time() - p1
+}
+
 
 # luetaan aikasarjat Euroopan Keskuspankin tilastorajapinnasta
 # Info: https://data.ecb.europa.eu/help/api/overview
@@ -12,21 +21,40 @@ library(odbc)
 # Dataset: Long-term interest rate for convergence purposes - 10 years maturity - EU27
 # Info: https://data.ecb.europa.eu/data/datasets/IRS/IRS.M.V5.L.L40.CI.0000.Z01.N.Z
 
-ecbKey <- ("IRS.M.V5.L.L40.CI.0000.Z01.N.Z")
-ecbFilter <- list(startPeriod = "1995-W01")
+# kaikki hakuavaimet maittain
 
-ecbData <- get_data(ecbKey, ecbFilter)
+ecbKeys <- c("IRS.M.V5.L.L40.CI.0000.Z01.N.Z","IRS.M.PL.L.L40.CI.0000.PLN.N.Z","IRS.M.RO.L.L40.CI.0000.RON.N.Z",
+             "IRS.M.SE.L.L40.CI.0000.SEK.N.Z","IRS.M.BG.L.L40.CI.0000.BGN.N.Z","IRS.M.CZ.L.L40.CI.0000.CZK.N.Z",
+             "IRS.M.DK.L.L40.CI.0000.DKK.N.Z","IRS.M.HU.L.L40.CI.0000.HUF.N.Z","IRS.M.GB.L.L40.CI.0000.GBP.N.Z")
 
-head(ecbData)
+# testikutsu, hakee ecbKeys viimeisimmän tunnisteen:
+ecbTest <- get_data(last(ecbKeys),ecbFilter)
 
-# luodaan ehjä DateKey -päiväyssarake ja lisätään sarakkeet datalähteelle ja datasetin tunnisteelle
 
-ecbData.Clean1 <- ecbData %>% mutate(date_key = as.integer(date(paste0(obstime,"-01")))) %>% 
-        mutate(datasource = "ecb") %>% mutate(dataset = "IRS.M.V5.L.L40.CI.0000.Z01.N.Z")
+ecbRegion <- c("EU27","PLN","RON","SEK","BGN","CZK","DKK","HUF","HRK","BGN","EEK","GBP","CZK","")
+ecbFilter <- list(startPeriod = "2000-W01")
 
-# poistetaan turhat sarakkeet
+# nouda EU27
 
+ecbData.Raw <- get_data(ecbKeys[1], ecbFilter)
+ecbData.Clean1 <- ecbData.Raw %>% mutate(date_key = as.integer(date(paste0(obstime,"-01")))) %>% 
+        mutate(datasource = "ecb", region = ecbRegion[1], dataset = ecbKeys[1])
 ecbData.Clean2 <- ecbData.Clean1 %>% select(datasource,dataset,obsvalue,date_key)
+ecbData <- ecbData.Clean2
+
+rm(ecbData.Raw,ecbData.Clean1,ecbData.Clean2)
+
+# nouda jäsenvaltiot
+
+for(i in 2:length(ecbKeys)) {
+        ecbData.Raw <- get_data(ecbKeys[i], ecbFilter)
+        ecbData.Clean1 <- ecbData.Raw %>% mutate(date_key = as.integer(date(paste0(obstime,"-01")))) %>% 
+                mutate(datasource = "ecb", region = ecbRegion[i], dataset = ecbKey)
+        
+        apause(1)
+}
+
+
 
 # ====== INSERT INTO DATABASE =======
 
@@ -37,6 +65,6 @@ dbExecute(con, schema = "dw", statement = s1)
 
 table_id <- Id(schema = "dw", table = "fact_ecb")
 
-dbWriteTable(con, table_id, ecbData.Clean2, overwrite = TRUE)
+dbWriteTable(con, table_id, ecbData, overwrite = TRUE)
 
 dbDisconnect(con)
