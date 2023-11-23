66ae0d03757f0819b072027d3dfeb878f03d85d8
Timo Ruohomaki <timo@ruohomaki.fi>
2023-11-23 22:15:40 +0200

    Update bof-open-data.R

--- dap-visu-etl/bof-open-data.R
+++ dap-visu-etl/bof-open-data.R
@@ -23,24 +23,4 @@ head(bofRates)
 
 bofRates[8]
 
-
-
-
-con <- DBI::dbConnect(odbc::odbc(), "dap-pgdb01")
-
-# for debugging purposes
-
-dbListTables(con, schema = "staging")
-dbListFields(con, schema = "staging", "bof-rates")
-
-
-rs <- dbSendQuery(con, schema = "staging", paste0("INSERT INTO staging.bof-rates",
-                                                  " (\"phenomenon_time\",",
-                                                  " \"country_region\")",
-                                                  " VALUES (\"",
-                                                  fileData[1]$`01-01-2021`$PhenomenonTime[1],
-                                                  "\",\"",
-                                                  fileData[1]$`01-01-2021`$Country_Region[1],
-                                                  "\")"))
-
-paste("Inserted", dbGetRowsAffected(rs) ,"rows.")
+# TO DO
