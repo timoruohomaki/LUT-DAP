9c9dd54fed8f3e8c8951832098b3154c22b7188e
Timo Ruohomaki <timo@ruohomaki.fi>
2023-12-11 22:10:18 +0200

    added script to prepare THL dental codes import

--- CT80A0000/dataIngest/thlcodeserver.R
+++ CT80A0000/dataIngest/thlcodeserver.R
@@ -1,14 +1,31 @@
 library(dplyr)
 library(readxl)
+library(redux)
+
+# lähde: https://koodistopalvelu.kanta.fi/codeserver/pages/classification-view-page.xhtml 
 
 dentalCodes.raw <- read_excel("./data/THLDentalCodes.xlsx")
 
 dentalCodes.fi <- dentalCodes.raw %>% select(Id,`Long name`)
+
+colnames(dentalCodes.fi) <- c("ID", "VALUE") 
+
+dentalCodes.import <- dentalCodes.fi %>% mutate(keyvalue = paste0("SET ", ID," '", VALUE, "'")) %>% select(keyvalue)
+
+write.table(dentalCodes.import, file = "./data/dentalCodes.csv", sep = ";", eol = "\n", row.names = FALSE, col.names = FALSE, quote = FALSE, fileEncoding = "utf8")
+
 dentalCodes.en <- dentalCodes.raw %>% select(Id,Long_name)
 
+# setting redis connection config
 
-# create key-value lists in Finnish and English
+redis_config(host = "redis-12727.c327.europe-west1-2.gce.redns.redis-cloud.com",
+             password = "4gWf01bPnabshmC5MtHFmfYRNDdOGUg3",port = "12727",
+             db = "codeServer", timeout = 30)
 
+r <- redux::hiredis()
 
-write.table(dentalCodes.fi, file = "./data/thlDentalCodesFi.csv", eol = "\r \n", col.names = TRUE, fileEncoding = "utf8")
-write.table(dentalCodes.en, file = "./data/thlDentalCodesEn.csv", eol = "\r \n", col.names = TRUE, fileEncoding = "utf8")
+redis <- redux::redis
+
+r$pipeline(
+    redis$PING(),
+    redis$PING())
