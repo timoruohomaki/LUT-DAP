eaa7305ca53f47fdd0dc50d8fda25ddba5877574
Timo Ruohomaki <timo@ruohomaki.fi>
2024-02-03 19:15:34 +0200

    initial draft

---
+++ CT60A4304/backend/crm.py
@@ -0,0 +1,27 @@
+#
+# CT60A4304 Basics of Database Systems
+# Course Project: CRM System
+#
+
+import sqlite3
+db = sqlite3.connect('crm.db')
+cur = db.cursor()
+
+def createLead():
+    
+    
+def convertLead():
+    
+    
+def createAccount():
+    
+    
+def getAccount():
+    
+    
+def getOpportunitySummary():
+    
+    
+def getContactList():
+    
+    
