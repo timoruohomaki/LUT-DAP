c0cd111f7049592f4ab92aef5bb887e7286958c7
timoruohomaki <timo@ruohomaki.fi>
2024-02-04 17:08:40 +0200

    minor updates

--- CT60A4304/backend/crm.py
+++ CT60A4304/backend/crm.py
@@ -3,12 +3,17 @@
 # Course Project: CRM System
 #
 
-import sqlite3
-db = sqlite3.connect('crm.db')
-cur = db.cursor()
 
 import sqlite3
 from sqlite3 import Error
+
+# from prettytable import PrettyTable
+# from prettytable import from_db_cursor
+
+db = sqlite3.connect('crm.db')
+cur = db.cursor()
+
+
 
 def initDB():
 
@@ -49,5 +54,40 @@ def getContactList():
     for row in rows:
         print(row)
 
-main()
-    initDB()
+def mainNavi():
+    print("")
+    print("===================")
+    print("=== CRM CONSOLE ===")
+    print("===================")
+    print("")
+    print("[1] - LIST ACCOUNTS")
+    print("[2] - LIST CONTACTS")
+    print("[3] - LIST NEW LEADS")
+    print("[4] - LIST NEW OPPORTUNITIES")
+    print("[5] - CREATE NEW LEAD")
+    print("[6] - CREATE NEW ACCOUNT")
+    print("[7] - CREATE NEW CONTACT")
+    print("[8] - CREATE NEW OPPORTUNITY")
+    print("[9] - EXIT")
+    print("===================")
+
+    mainSelect = int(input("Select option (1-9):"))
+
+    match mainSelect:
+        case 1:
+            print("Listing accounts")
+            mainNavi()
+        case 2:
+            print("Listing contacts")
+        case 3:
+            print("Listing new leads")
+        case 4:
+            print("Listing new opportunities")
+        case 9:
+            print("Bye!")
+
+# MAIN PROGRAM STARTS HERE
+
+mainNavi()
+
+
