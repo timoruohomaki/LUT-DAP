3bf87f22be5813267a51370b656ce44c3fdc064d
Timo Ruohomaki <timo@ruohomaki.fi>
2024-10-24 17:31:19 +0300

    added happyornot test data creation script

---
+++ CS37A0130/happyornot/happyornot.R
@@ -0,0 +1,51 @@
+library(lubridate)
+library(cleaner)
+library(dplyr)
+library(ggplot2)
+library(jsonlite)
+
+if(!file.exists("./data")){dir.create("./data")}
+
+rowCount <- 10000
+rangeBegin <- as.Date("2024-01-01")
+rangeEnd <- as.Date("2024-11-23")
+
+dateArray <- rdate(rowCount,rangeBegin, rangeEnd)
+dateArray <- sort(dateArray, decreasing = FALSE)
+
+s <- 1:5
+
+timeArray <- paste0(sprintf("%02d",(sample((8:16), rowCount, replace = TRUE))),":",
+                    sprintf("%02d",(sample((0:59), rowCount, replace = TRUE))),":",
+                    sprintf("%02d",(sample((0:59), rowCount, replace = TRUE))))
+
+siteArray <- sample(s, rowCount, replace = TRUE, prob = c(0.4,0.3,0.1,0.1,0.1))
+
+b <- c(1.0,0.97,0.94,0.91,0.88,0.83,0.79,0.74,0.71,0.67)
+
+battLevelArray <- sample(b, rowCount, replace = TRUE)
+battLevelArray <- sort(battLevelArray, decreasing = TRUE)
+
+x <- 1:5
+feedbackArray <- sample(x, rowCount, replace = TRUE, prob = c(0.05,0.1,0.3,0.35,0.2))
+
+feedback.raw <- data.frame(siteArray, dateArray, timeArray, feedbackArray, battLevelArray)
+colnames(feedback.raw) <- c("SiteID","ObsDate","ObsTime","Feedback","BatteryLevel")
+
+feedback.final <- feedback.raw %>% mutate(ObservedAt = paste(dateArray,timeArray,sep='T'))
+feedback.final <- feedback.final %>% arrange(ymd_hms(ObservedAt))
+
+# feedback.final <- feedback.raw %>% arrange(ObservedAt)
+feedback.final$Feedback <- as.factor(feedback.final$Feedback)
+
+feedback.final <- feedback.final %>% filter(SiteID,Feedback,BatteryLevel,ObservedAt)
+
+# testing distribution by plotting it
+ggplot(data.frame(feedback.final), aes(x=Feedback)) +
+  geom_bar(fill="lightgreen")
+
+# exporting json
+
+write_json(feedback.final, "./data/happyornot_2024.json")
+
+tail(feedback.final)
