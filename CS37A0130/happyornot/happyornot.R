3cff08e07097c1d9295418124c3dcf7239d5504c
timoruohomaki <timo@ruohomaki.fi>
2024-11-17 17:10:33 +0200

    fixes in json structure, now successfully imports on IoTTicket

--- CS37A0130/happyornot/happyornot.R
+++ CS37A0130/happyornot/happyornot.R
@@ -20,13 +20,13 @@ rangeEnd <- as.Date("2024-11-30")
 dateArray <- rdate(rowCount,rangeBegin, rangeEnd)
 dateArray <- sort(dateArray, decreasing = FALSE)
 
-s <- 1:5
+s <- c(1000,1001,1002,1037,1526,1580,1621,1648,1713,1720)
 
 timeArray <- paste0(sprintf("%02d",(sample((8:16), rowCount, replace = TRUE))),":",
                     sprintf("%02d",(sample((0:59), rowCount, replace = TRUE))),":",
                     sprintf("%02d",(sample((0:59), rowCount, replace = TRUE))),".000Z")
 
-siteArray <- sample(s, rowCount, replace = TRUE, prob = c(0.4,0.3,0.1,0.1,0.1))
+siteArray <- sample(s, rowCount, replace = TRUE, prob = c(0.2,0.2,0.1,0.05,0.1,0.05,0.1,0.05,0.1,0.05))
 
 b <- c(1.0,0.97,0.94,0.91,0.88,0.83,0.79,0.74,0.71,0.67)
 
@@ -42,7 +42,7 @@ colnames(feedback.raw) <- c("SiteID","ObsDate","ObsTime","Feedback","BatteryLeve
 feedback.final <- feedback.raw %>% mutate(ObservedAt = paste(dateArray,timeArray,sep='T'))
 feedback.final <- feedback.final %>% arrange(ymd_hms(ObservedAt))
 
-feedback.today <- feedback.final %>% filter(ObsDate == "2024-11-16")
+feedback.today <- feedback.final %>% filter(ObsDate == "2024-11-17")
 
 # testing distribution by plotting it
 ggplot(data.frame(feedback.today), aes(x=Feedback)) +
@@ -72,17 +72,38 @@ Sys.getenv("apiUser")
 
 # create JSON structure for API call
 
-apiData <- toJSON(feedback.today, pretty = TRUE)
+feedback.feedback <- setNames(as.list(feedback.today$Feedback), feedback.today$ObservedAt)
+feedback.battery <- setNames(as.list(feedback.today$BatteryLevel), feedback.today$ObservedAt)
+feedback.sites <- setNames(as.list(feedback.today$SiteID), feedback.today$ObservedAt)
 
-apiResult <- PUT(url = Sys.getenv("apiEventURI"),
+fbacks <- list(feedback.feedback)
+fbatts <- list(feedback.battery)
+fsites <- list(feedback.sites)
+
+lelem1=list(n="Feedback", dt="double", unit="happiness", data = fbacks)
+lelem2=list(n="BatteryLevel", dt="double", unit="", data = fbatts)
+lelem3=list(n="SiteID", dt="string", unit="", data = fsites)
+
+ls=list(t=list(lelem1, lelem2, lelem3))
+
+toiot.json <- toJSON(ls, auto_unbox = TRUE, pretty = TRUE) %>% jsonlite::prettify()
+
+toiot.json
+
+# post JSON on IoT-Ticket
+
+Sys.getenv("apiTelemetryURI")
+
+apiResult <- PUT(url = Sys.getenv("apiTelemetryURI"),
                   authenticate(Sys.getenv("apiUser"),
                                Sys.getenv("apiPwd"),
                                type = "basic"),
-                  body = apiData,
+                  body = toiot.json,
                   content_type_json(),
                  user_agent(Sys.getenv("UserAgent"))
                 )
 
 # verify success
 status_code(apiResult)
+apiResult$url
 
