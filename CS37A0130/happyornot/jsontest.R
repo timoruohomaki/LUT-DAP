ba70d52fee9850c925be2105185b55afc4bb9824
Timo Ruohomaki <timo@ruohomaki.fi>
2024-11-23 08:48:33 +0200

    added function

--- CS37A0130/happyornot/jsontest.R
+++ CS37A0130/happyornot/jsontest.R
@@ -1,4 +1,18 @@
 library(dplyr)
 library(jsonlite)
 
-toJSON(list(n = "Temperature", unit = "Celsius", data=list(list("foo" = "bar"), list("baz"="qux"), list("fred" = "thud"))),auto_unbox = TRUE) %>% jsonlite::prettify()
+getData <- function() {
+  
+  dataList <- list()
+  
+  for (i in cars) {
+    dataList <- append(dataList,list(list("a"=i$speed),list("b"=i$dist)))
+  }
+  
+  return(dataList)
+  
+}
+
+toJSON(list(n = "Temperature", unit = "Celsius", list(data=getData())), auto_unbox = TRUE) %>% jsonlite::prettify()
+
+# toJSON(list(n = "Temperature", unit = "Celsius", data=list(list("foo" = "bar"), list("baz"="qux"), list("fred" = "thud"))),auto_unbox = TRUE) %>% jsonlite::prettify()
